38f5e8d0d0ceaab5f630d86a7b7170da7ab923ed
SmritiSrivastava08 <83022838+SmritiSrivastava08@users.noreply.github.com>
2021-09-30 14:27:47 +0530

    Add files via upload

---
+++ Login.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="True" CodeBehind="Login.aspx.cs" Inherits="BookStore.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="css/Custom.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="centerpage">
+            <h1>Login Form</h1>
+            <asp:TextBox ID="txtUname1" runat="server" CssClass="class2" placeholder="Enter Your Username"></asp:TextBox>
+
+            <asp:RequiredFieldValidator ID="RequiredFieldValidatorUname" runat="server" ErrorMessage="<br/>Please Enter Username<br/>" Cssclass="text-danger" ControlToValidate="txtUname1" ForeColor="Red"></asp:RequiredFieldValidator>
+            
+            <asp:TextBox ID="txtPass1" runat="server" TextMode="Password" CssClass="class2" placeholder="Enter Your Password"></asp:TextBox>
+
+            <asp:RequiredFieldValidator ID="RequiredFieldValidatorPassword" runat="server" ErrorMessage="<br/>Please Enter Password" Cssclass="text-danger" ControlToValidate="txtPass1" ForeColor="Red"></asp:RequiredFieldValidator>
+            <br />
+            <asp:Button ID="btnLogin" runat="server" Text="Login" CssClass="class1" OnClick="Button1_Click"/>
+            <div>OR</div>
+            <asp:Button ID="btnSignUp" runat="server" Text="SignUp" CssClass="class1" OnClick="btnSignUp_Click" CausesValidation="false" />
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BookStore
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["BookstoreDB"].ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from UserDetails where Username=@uname and Password=@pwd", con);
+                cmd.Parameters.AddWithValue("@uname", txtUname1.Text);
+                cmd.Parameters.AddWithValue("@pwd", txtPass1.Text);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count != 0)
+                {
+                    Session["Username"] = txtUname1.Text;
+                    Response.Redirect("AfterLogin.aspx");
+                }
+                else
+                {
+                    Response.Write("<script> alert('Invalid Username or Password'); </script>");
+                }
+                con.Close();
+            }
+            
+        }
+
+        protected void btnSignUp_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("signup.aspx");
+        }
+    }
+}
