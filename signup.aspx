558fc8bce97f8cac303f6734486ff0ed400281c6
SmritiSrivastava08 <83022838+SmritiSrivastava08@users.noreply.github.com>
2021-09-30 14:30:59 +0530

    Add files via upload

---
+++ signup.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Inherits="BookStore.signup" Codebehind="signup.aspx.cs" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Signup</title> 
+    <link href="css/Custom.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="centerpage">
+            <h1>Registeration Form</h1>
+            <asp:TextBox ID="txtUname" runat="server" CssClass="class2" placeholder="Your Name"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtEmail" runat="server" CssClass="class2" placeholder="Your Email"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtPass" runat="server" TextMode="Password" CssClass="class2" placeholder="Your Password"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtCPass" runat="server" TextMode="Password" CssClass="class2" placeholder="Re-enter Password"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtPhone" runat="server" CssClass="class2" placeholder="Your Phone Number"></asp:TextBox>
+            <br />
+            <asp:Button ID="txtsignup" runat="server" Text="Sign Up" CssClass="class1" OnClick="txtsignup_Click1" />
+            <div>OR</div>
+            <asp:Button ID="btnLogin" runat="server" Text="Login" CssClass="class1" OnClick="btnLogin_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ signup.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace BookStore
+{
+    public partial class signup : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        protected void txtsignup_Click1(object sender, EventArgs e)
+        {
+          
+
+            if(isformvalid())
+            {
+                using(SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings ["BookstoreDB"].ConnectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Insert into UserDetails(Username,Password,Email,Phone) Values('" + txtUname.Text + "','" + txtPass.Text + "','" + txtEmail.Text + "','" + txtPhone.Text + "')", con);
+                    cmd.ExecuteNonQuery();
+                    //Response.Write("<script> alert('Registration Successfully Done, Please login now'); </script>");
+                    Response.Write("<script>alert('Registration Successfully Done, Please login now');</script>");
+                    Server.Transfer("login.aspx");
+                    con.Close();
+                }
+                //Response.Redirect("login.aspx");
+            }
+        }
+        private bool isformvalid()
+        {
+            if((txtUname.Text=="") || (txtPass.Text == "") || (txtCPass.Text == "") || (txtEmail.Text == "") || (txtPhone.Text == ""))
+            {
+                Response.Write("<script> alert('Please provide all details'); </script>");
+                return false;
+            }
+            else if(txtPass.Text!=txtCPass.Text)
+            {
+                Response.Write("<script> alert('Password and confirm password not matching'); </script>");
+                return false;
+            }
+            return true;
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
