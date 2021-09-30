318698cf6d1b8d49eb210a679248dc5399093a8e
SmritiSrivastava08 <83022838+SmritiSrivastava08@users.noreply.github.com>
2021-09-30 14:59:10 +0530

    Add files via upload

---
+++ AfterLogin.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="True" CodeBehind="AfterLogin.aspx.cs" Inherits="BookStore.AfterLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="css/Custom.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblSuccess" runat="server" ></asp:Label>
+            <asp:Button ID="btnLogout"  runat="server" Text="Logout" CssClass="class3" OnClick="btnLogout_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AfterLogin.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BookStore
+{
+    public partial class AfterLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["Username"]!=null)
+            {
+                lblSuccess.Text = "Welcome " + Session["Username"].ToString();
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Session.Remove("Username");
+            Response.Redirect("~/Login.aspx");
+        }
+    }
+}
